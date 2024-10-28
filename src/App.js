71706a0d57f12ab3272ed55e508c2bf6922baa32
Zavyalo <pngzavyalova@yandex.ru>
2024-10-28 11:13:54 +0500

    link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import {Header} from "./components/header";
 import {Grid} from "./components/grid";
 // import {Cards} from "./components/cards";
 import {Footer} from "./components/footer";
+import {FirstPage} from "./components/FirstPage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Grid />
-      <Footer />    
-    </div>
+    // <div className="App">
+    //   <Header />
+    //   <FirstPage /> 
+    //   <Grid />
+    //   <Footer />    
+    // </div>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FirstPage/>} />
+          <Route path="/play" element={<Header/>} />
+          <Route path="/play" element={<Grid/>} />
+          <Route path="/play" element={<Footer/>} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/FirstPage.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from 'react-router-dom';
+
+export function FirstPage(){
+    return(
+    // <nav>
+    // <NavLink to="/" className={({ isActive }) => (isActive ? 'link link--active' : 'link')}>Начать игру</NavLink>
+    // </nav>
+    <h1>Начать игру</h1>
+    )
+}
+
+// function Nav() {
+//     return <nav>
+//         <Link to "/play"> Начать игру </Link>
+//     </nav>;
+// }
